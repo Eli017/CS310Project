6074da78f0340ef8d5e0bfb83afcb15b46d9db64
Eli Sokeland <etsokeland@bsu.edu>
2018-10-11 00:26:11 -0400

    Added functions to userFeed.js

--- javascript/userFeedData.js
+++ javascript/userFeedData.js
@@ -1,8 +1,7 @@
-function Post(userImage, userName, title, media, text) {
+function Post(userImage, userName, title, text) {
     this.userImage = userImage;
     this.userName = userName;
     this.title = title;
-    this.media = media;
     this.text = text;
 }
 
@@ -10,7 +9,6 @@ let post1 = new Post(
     "profileFiller.jpg",
     "John Doe",
     "Post Title 1",
-    null,
     "This is an example of a post without any media and only text."
 );
 
@@ -18,14 +16,14 @@ let post2 = new Post(
     "profileFiller.jpg",
     "Jane Smith",
     "Post Title 2",
-    "mukip.gif",
-    "This is an example of a post with media, such as a GIF."
+    "This is an example of another post."
 );
 
 let post3 = new Post(
     "profileFiller.jpg",
     "Peter Parker",
-    "Post Without Text",
-    "postExample2.jpg",
-    null
+    "Post 3",
+    "Post",
 );
+
+let allPosts = [post1, post2, post3];
--- javascript/userFeedFunctions.js
+++ javascript/userFeedFunctions.js
@@ -1,3 +1,18 @@
-function outputPost(userImage, userName, title, media, text) {
-    document.write("<div class='post'>")
+function outputPost(userImage, userName, title, text) {
+        document.write("<article class='post'>");
+            document.write("<img src='../images/"+userImage+"' alt='Profile Picture' title='Profile Picture' class='profilePicture'>");
+            document.write("<h4>"+userName+"</h4>");
+            document.write("<h5 class='title'>"+title+"</h5>");
+            document.write("<hr/>");
+            document.write("<p>"+text+"</p>");
+        document.write("</article>");
+}
+
+function displayAllPosts() {
+    document.write("<section class='allPosts'>");
+    for (let i = 0; i < allPosts.length; i++) {
+        let currentPost = allPosts[i];
+        outputPost(currentPost.userImage, currentPost.userName, currentPost.title, currentPost.text);
+    }
+    document.write("</section>");
 }
