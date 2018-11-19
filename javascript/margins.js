463d526bebe46dabd86a431007f1a1a9b942ce1f
Eli Sokeland <etsokeland@bsu.edu>
2018-11-19 09:46:51 -0500

    Added header and footer function to html

--- javascript/margins.js
+++ javascript/margins.js
@@ -1,20 +1,25 @@
-import "userpost";
-
 function outputHeader() {
     document.write('<header>');
-    document.write('<a href="userFeed.html"><img src="../images/Logo1.png" title="Agora Logo" alt="Agora Logo" id="logo"/></a>');
-    document.write('<h3><a href="userFeed.html">Agora</a></h3>');
+    document.write('<a href="../html/userFeed.html"><img src="../images/Logo1.png" title="Agora Logo" alt="Agora Logo" id="logo"/></a>');
+    document.write('<h3><a href="../html/userFeed.html">Agora</a></h3>');
     document.write('<form>');
     document.write('<label>Search: </label>');
     document.write('<input type="text" title="Search" placeholder="Input Keywords for Search"/>');
     document.write('<input type="submit" title="SubmitSearch" value="Search"/>');
-    welcomeUser("user"); //Will change based on user
+    // welcomeUser("user"); //Will change based on user
     document.write('</form>');
-    document.write('<form action="login.html" class="userLoginRegister">');
+    document.write('<form action="../html/login.html" class="userLoginRegister">');
     document.write('<input type="submit" value="Login" title="Login"/>');
     document.write('</form>');
-    document.write('<form action="register.html" class="userLoginRegister">');
+    document.write('<form action="../html/register.html" class="userLoginRegister">');
     document.write('<input type="submit" value="Register" title="Register"/>');
     document.write('</form>');
-    document.write('</form>');
+    document.write('</header>');
+}
+
+function outputFooter() {
+    document.write("<footer>");
+    document.write("<p>&copy Bolles, Runnebohm, Sokeland, and Stull</p>");
+    document.write("<p>Built for CS310 Project</p>");
+    document.write("</footer>");
 }
