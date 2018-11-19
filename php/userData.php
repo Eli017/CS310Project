9e1e752b5e722591e74d3645ca0c92ab137d7fd5
Eli Sokeland <etsokeland@bsu.edu>
2018-11-19 15:17:51 -0500

    Added two user arrays in userData.php and refactored files.

--- php/userData.php
+++ php/userData.php
@@ -1,1 +1,14 @@
 <?php
+$user1 = array(
+    "name" => "John Doe",
+    "profilePhoto" => "user-face.jpg",
+    "originDate" => "April 24, 2017",
+    "score" => "6"
+);
+
+$user2 = array(
+    "name" => "Jane Smith",
+    "profilePhoto" => "user-face.jpg",
+    "originDate" => "April 24, 2017",
+    "score" => "6"
+);
