08474586cd13a6dd6e2c30bcce1dea78ba98a22d
Eli Sokeland <etsokeland@bsu.edu>
2018-11-20 03:26:28 -0500

    Added additional users and profile images for them.

--- php/userData.php
+++ php/userData.php
@@ -1,5 +1,6 @@
 <?php
 $user1 = array(
+    "id" => "1",
     "name" => "John Doe",
     "profilePhoto" => "user-face.jpg",
     "originDate" => "April 24, 2017",
@@ -7,8 +8,19 @@ $user1 = array(
 );
 
 $user2 = array(
+    "id" => "2",
     "name" => "Jane Smith",
-    "profilePhoto" => "user-face.jpg",
-    "originDate" => "April 24, 2017",
-    "score" => "6"
+    "profilePhoto" => "user2-face.jpg",
+    "originDate" => "May 15, 2015",
+    "score" => "8"
 );
+
+$user3 = array(
+    "id" => "3",
+    "name" => "Peter Parker",
+    "profilePhoto" => "user3-face.jpg",
+    "originDate" => "June 7, 2018",
+    "score" => "3"
+);
+
+$users = array($user1, $user2, $user3);
