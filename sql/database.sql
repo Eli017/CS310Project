3874df2347509ab373e278d5c998d5bf5ab8f4bb
austinbolles <asbolles@bsu.edu>
2018-12-10 02:09:52 -0500

    As requested by Eli

--- sql/database.sql
+++ sql/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Dec 10, 2018 at 01:19 AM
+-- Generation Time: Dec 10, 2018 at 07:07 AM
 -- Server version: 5.7.23
 -- PHP Version: 7.2.8
 
